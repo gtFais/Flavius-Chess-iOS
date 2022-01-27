4bf40e556e7d8863c0aace9a05bf71937bc451de
Flavius <flv.trc@outlook.com>
2022-01-27 11:35:44 +0200

    create a preview of the chessboard

---
+++ Chess/BoardView.swift
@@ -0,0 +1,20 @@
+//
+//  BoardView.swift
+//  Chess
+//
+//  Created by Flavius on 27.01.2022.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
